1488ca7f2046afff87e8af507ef188093b9fd12f
Mason021 <mdc5054@gmail.com>
2021-10-31 10:49:24 -0500

    updated my server.js and auth file in utils redirect route

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -24,13 +24,11 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log("Invalid or bad formatted token");
     }
-
-    // send to next endpoint
-    next();
+    return req;
   },
+  
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
